877bdee1bb8d8885b8b19555337d7915e0dbb244
alexabreusantos <alexdeabreudossantos@gmail.com>
2016-06-16 09:40:25 -0300

    Modificação na classe DAO

--- app/src/main/java/com/alexabreu/minhasletras/dao/LetraDAO.java
+++ app/src/main/java/com/alexabreu/minhasletras/dao/LetraDAO.java
@@ -9,89 +9,81 @@ import android.util.Log;
 import com.alexabreu.minhasletras.helper.DBHelper;
 import com.alexabreu.minhasletras.model.Letra;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by alexd on 08/06/2016.
  */
 public class LetraDAO {
 
     private DBHelper dbHelper;
-    private static final String TAG = "cadastro_letra";
     private Context context;
+    private static final String TAG = "cadastro_letra";
+    private static final String nome_tabela = "letras";
 
     public LetraDAO(Context context) {
         dbHelper = new DBHelper(context);
         this.context = context;
     }
 
-    public int insert(Letra letra) {
+    public void inserir(Letra letra) {
 
         //Open connection to write data
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("nome_musica", letra.getNome_musica());
-        values.put(Letra.KEY_cantor,letra.getNome_cantor());
-        values.put(Letra.KEY_letra, letra.getLetra_musica());
+        values.put("cantor_musica",letra.getCantor_musica());
+        values.put("letra_musica", letra.getLetra_musica());
 
         // Inserting Row
-        long letra_Id = db.insert(Letra.TABLE, null, values);
+        long id_letra = db.insert(nome_tabela, null, values);
         db.close(); // Closing database connection
-        return (int) letra_Id;
     }
 
-    public boolean update(String id, String nome, String cantor, String letra){
+   public void atualizar(Letra letra){
+       ContentValues values = new ContentValues();
+       values.put("nome_musica", letra.getNome_musica());
+       values.put("cantor_musica",letra.getCantor_musica());
+       values.put("letra_musica", letra.getLetra_musica());
+       SQLiteDatabase db =  dbHelper.getWritableDatabase();
+       db.update(nome_tabela, values, "_id_musica=?", new String[]{letra.getId_musica().toString()});
+       db.close();
+
+   }
+
+    public void remover(Letra letra){
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(Letra.KEY_ID, id);
-        values.put(Letra.KEY_nome, nome);
-        values.put(Letra.KEY_cantor, cantor);
-        values.put(Letra.KEY_letra, letra);
-
-        db.update(Letra.TABLE, values, Letra.KEY_ID + "=?", new String[]{id});
-
-        return true;
+        db.delete(nome_tabela, "_id_musica=?", new String[]{letra.getId_musica().toString()});
+        db.close();;
     }
 
-    public int delet(Long id){
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        int rows = db.delete(Letra.TABLE, "id = ?",new String[]{ String.valueOf(id) });
-        return rows; // qtde. de linhas afetadas
-    }
-
-    public Cursor getLetraList() {
-        //Open connection to read only
+    public List<Letra> listarTodos() {
+        List<Letra> letras = new ArrayList<Letra>();
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        String selectQuery =  "SELECT  rowid as " +
-                Letra.KEY_ROWID + "," +
-                Letra.KEY_ID + "," +
-                Letra.KEY_nome + "," +
-                Letra.KEY_cantor + "," +
-                Letra.KEY_letra +
-                " FROM " + Letra.TABLE + " ORDER BY " + Letra.KEY_nome;
+        Cursor c = db.query(nome_tabela, null, null, null, null, null, "nome_musica ASC");
 
-
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        // looping through all rows and adding to list
-
-        if (cursor == null) {
-            return null;
-        } else if (!cursor.moveToFirst()) {
-            cursor.close();
-            return null;
+        try {
+            while (c.moveToNext()){
+                Letra letra = new Letra();
+                letra.setId_musica(c.getLong(c.getColumnIndex("_id_musica")));
+                letra.setNome_musica(c.getString(c.getColumnIndex("nome_musica")));
+                letra.setCantor_musica(c.getString(c.getColumnIndex("cantor_musica")));
+                letra.setLetra_musica(c.getString(c.getColumnIndex("letra_musica")));
+                letras.add(letra);
+            }
+        }finally {
+            c.close();
         }
-        return cursor;
+        db.close();
+        return letras;
     }
 
     public Cursor getLetraListByNome(String search) {
         //Open connection to read only
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        String selectQuery =  "SELECT  rowid as " +
-                Letra.KEY_ROWID + "," +
-                Letra.KEY_ID + "," +
-                Letra.KEY_nome + "," +
-                Letra.KEY_cantor + "," +
-                Letra.KEY_letra +
-                " FROM " + Letra.TABLE +
-                " WHERE " +  Letra.KEY_nome + "  LIKE  '%" +search + "%' ";
+        String selectQuery =  "SELECT nome_musica, cantor_musica FROM " + nome_tabela + " WHERE nome_musica LIKE '%" +search + "%' ";
+        
         Cursor cursor = db.rawQuery(selectQuery, null);
         // looping through all rows and adding to list
 
@@ -104,38 +96,7 @@ public class LetraDAO {
         return cursor;
     }
 
-    public Letra getLetraById(int Id){
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-        String selectQuery =  "SELECT " +
-                Letra.KEY_ID + "," +
-                Letra.KEY_nome + "," +
-                Letra.KEY_cantor + "," +
-                Letra.KEY_letra +
-                " FROM " + Letra.TABLE
-                + " WHERE " +
-                Letra.KEY_ID + "=?";// It's a good practice to use parameter ?, instead of concatenate string
-
-        int iCount =0;
-        Letra letra = new Letra();
-
-        Cursor cursor = db.rawQuery(selectQuery, new String[] { String.valueOf(Id) } );
-
-        if (cursor.moveToFirst()) {
-            do {
-                letra.setLetra_ID(cursor.getLong(cursor.getColumnIndex(Letra.KEY_ID)));
-                letra.setNome_musica(cursor.getString(cursor.getColumnIndex(Letra.KEY_nome)));
-                letra.setNome_cantor(cursor.getString(cursor.getColumnIndex(Letra.KEY_cantor)));
-                letra.setLetra_musica(cursor.getString(cursor.getColumnIndex(Letra.KEY_letra)));
-
-            } while (cursor.moveToNext());
-        }
-
-        cursor.close();
-        db.close();
-        return letra;
-    }
-
-    public String obterNomeMusica(Cursor cursor){
-        return cursor.getString(1);
+    public String obterNomeMusica(Letra letra){
+        return letra.getNome_musica();
     }
 }
